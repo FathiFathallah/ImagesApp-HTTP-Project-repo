cab84702fefbd19758636005f2b24aa2c624afdf
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-02-24 02:02:20 +0200

    DataBase Table for images

---
+++ DataBase.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `images` (
+ `Id` int(11) NOT NULL,
+ `name` varchar(45) NOT NULL,
+ `Description` varchar(500) NOT NULL,
+ UNIQUE KEY `ID` (`Id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4	
